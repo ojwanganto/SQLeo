b38ff4d7ff222c8869dddb47cc4bcf6810519f37
legrand_legrand <legrand_legrand@a56d395e-7d1e-48a7-b67f-404d3d643c0c>
2015-03-26 20:58:10 +0000

    ticket #256 Data comparer: columns names with special chars and diff status

--- src/com/sqleo/environment/ctrl/DataComparer.java
+++ src/com/sqleo/environment/ctrl/DataComparer.java
@@ -248,13 +248,13 @@ public class DataComparer extends BorderLayoutPanel
 		final String realSourceAlias = sourceAlias!=null?sourceAlias:"SOURCE";
 		final String realTargetAlias = targetAlias!=null?targetAlias:"TARGET";
 		for(int i = 1; i<=totalAggregates; i++){
-			result.append("MAX(SRC").append(i).append(") as agg").append(i).append("_").append(realSourceAlias).append(",");
-			result.append("MAX(TGT").append(i).append(") as agg").append(i).append("_").append(realTargetAlias);
+			result.append("MAX(SRC").append(i).append(") as \"agg").append(i).append("_").append(realSourceAlias).append("\",");
+			result.append("MAX(TGT").append(i).append(") as \"agg").append(i).append("_").append(realTargetAlias).append("\"");
 			// ticket #260 add diff status for each line (can help when exporting in excel AND pitvot table
 			if(addDiffStatusInOutput.isSelected()){
-				result.append(",\n").append(" case when MAX(SRC").append(i).append(")='' then '").append(realTargetAlias).append("'");
+				result.append(",\n").append(" case when MAX(SRC").append(i).append(")=").append("MAX(TGT").append(i).append(")||'' then 'EQU'");
 				result.append(" when MAX(TGT").append(i).append(")='' then '").append(realSourceAlias).append("'");	
-				result.append(" when MAX(SRC").append(i).append(")=").append("MAX(TGT").append(i).append(")||'' then 'EQU'");	
+				result.append(" when MAX(SRC").append(i).append(")='' then '").append(realTargetAlias).append("'");	
 				result.append(" else 'DIFF' end as DIFF").append(i);	
 			}
 			if(i<totalAggregates){
