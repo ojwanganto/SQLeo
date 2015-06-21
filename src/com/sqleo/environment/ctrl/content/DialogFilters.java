7fdb81fef51000acef614e6c9c0d7568af367817
anudeepgade <anudeepgade@a56d395e-7d1e-48a7-b67f-404d3d643c0c>
2015-06-21 19:02:57 +0000

    Ticket #173 Fix NPE

--- src/com/sqleo/environment/ctrl/content/DialogFilters.java
+++ src/com/sqleo/environment/ctrl/content/DialogFilters.java
@@ -85,7 +85,9 @@ public class DialogFilters extends AbstractDialogConfirm implements _ClauseOwner
 				
 				append = (rowdata[3]!=null ? rowdata[3].toString() : null);
 			}
-			content.getClientQB().getQueryBuilder().setQueryModel(content.getQueryModel());
+			if(content.getClientQB()!=null){
+				content.getClientQB().getQueryBuilder().setQueryModel(content.getQueryModel());
+			}
 			content.getActionMap().get("task-go").actionPerformed(null);
 		}
 		
